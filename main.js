032a780d5b45e0d9e7b61a96106d426b6a4a466a
Ferdilando <ferdilando@hotmail.com>
2023-02-12 18:04:30 -0300

    cambio de nombres

---
+++ main.js
@@ -0,0 +1,35 @@
+// funcion que permite cargar datos de los empleados
+function cargar(){
+    var itemsACargar = document.getElementById("carga").value;
+    var i
+    for (i=1; i<=itemsACargar; i++) {
+        nombre = prompt("Ingrese el nombre de la persona "+i)
+        while (nombre === "" ){
+            alert("Ingrese un valor válido")
+            nombre = prompt("Ingrese el nombre de la persona "+i)
+        }
+        if (nombre === null) {
+            break
+        }
+        apellido = prompt("Ingrese el apellido de la persona "+i)
+        while (apellido === ""){
+            alert("Ingrese un valor válido")
+            apellido = prompt("Ingrese el apellido de la persona "+i)
+        }
+        if (apellido === null) {
+            break
+        }
+        dni = prompt("Ingrese el DNI de la persona "+i)
+        while (dni === "" || isNaN(dni)){
+            alert("Ingrese un valor válido")
+            dni = prompt("Ingrese el dni de la persona "+i)
+        }
+        if (dni === null) {
+            break
+        }
+        
+        alert("Se ingresaron los datos del empleado "+i+ ":"+" \n Nombre: "+nombre+" \n Apellido: "+apellido+" \n DNI: "+dni)}
+
+    }
+    
+      
