fb955301b1c90aedc96e3ca284f3c5e69c91f677
Fernando Vega <51489088+Ferdilando@users.noreply.github.com>
2023-03-04 20:25:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,57 +12,41 @@ class persona {
     }
 };
 
-// funcion que permite cargar datos de los empleados y los agrega al array 'empleados'
-function cargar() {
+// funcion que permite cargar datos de los empleados, imprimirlos sobre el HTML y los agrega al array 'empleados'
+function addEmployee() {
+    var nombre = document.getElementById("fname").value;
+    var apellido = document.getElementById("lname").value;
+    var dni = document.getElementById("dni").value;
 
-    // Se traen la cantidad de empleados que se quieran cargar al hacer click en el boton cargar
-    var itemsACargar = document.getElementById("carga").value;
-    var i
+    // se valida que no falte ningun dato
+    if (!nombre || !apellido || !dni) {
+        alert("Faltan datos")
+    } else {
+        // se insertan los datos del input en la tabla
+        var table = document.getElementsByName("tablapersona")[0];
+        var newRow = table.insertRow(1);
 
-    // se recorre la cantidad de empleados que se selecciono cargar
-    for (i = 1; i <= itemsACargar; i++) {
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
 
-        // se pide nombre, apellido y dni de la persona. No permite valores vacios ni que el dni contenga valores no numericos
-        nombre = prompt("Ingrese el nombre de la persona " + i)
-        while (nombre === "") {
-            alert("Ingrese un valor válido")
-            nombre = prompt("Ingrese el nombre de la persona " + i)
-        }
-        if (nombre === null) {
-            break
-        }
-        apellido = prompt("Ingrese el apellido de la persona " + i)
-        while (apellido === "") {
-            alert("Ingrese un valor válido")
-            apellido = prompt("Ingrese el apellido de la persona " + i)
-        }
-        if (apellido === null) {
-            break
-        }
-        dni = prompt("Ingrese el DNI de la persona " + i)
-        while (dni === "" || isNaN(dni)) {
-            alert("Ingrese un valor válido")
-            dni = prompt("Ingrese el dni de la persona " + i)
-        }
-        if (dni === null) {
-            break
-        }
-
-        // Se muestra los datos que se cargaron para corroborar
-        alert("Se ingresaron los datos del empleado " + i + ":" + " \n Nombre: " + nombre + " \n Apellido: " + apellido + " \n DNI: " + dni)
+        cell1.innerHTML = nombre;
+        cell2.innerHTML = apellido;
+        cell3.innerHTML = dni;
 
         // se pushea el nuevo objeto dentro del array vacio que habiamos creado
         empleados.push(new persona(nombre, apellido, dni))
-
     }
 }
+
+
 // Funcion para agregar el valor hora de algun empleado
 function valorHora() {
     // se obtiene el dni de la persona a la que se le quiere cargar el valor hora al hacer click en el boton
-    var valor = document.getElementById("valorHora").value
+    var dni = document.getElementById("valorHora").value
     // se mapea el array y se busca el objeto que coincida con el dni cargado
     var dnis = empleados.map(function (c) { return c.dni })
-    var persona = dnis.indexOf(valor)
+    var persona = dnis.indexOf(dni)
 
     // como el metodo indexOf trae como valor -1 en caso de que no encuentre el valor dentro del array, se crea un if para que solo se cargue la info en caso de haber coincidencia
     if (persona !== -1) {
@@ -86,10 +70,10 @@ function valorHora() {
 // Funcion para agregar cantidad de horas trabajadas de algun empleado
 function cantidadHoras() {
     // se obtiene el dni de la persona a la que se le quiere cargar la cantidad de horas trabajadas al hacer click en el boton
-    var valor = document.getElementById("cantidadHoras").value
+    var dni = document.getElementById("valorHora").value
     // se mapea el array y se busca el objeto que coincida con el dni cargado
     var dnis = empleados.map(function (c) { return c.dni })
-    var cHoras = dnis.indexOf(valor)
+    var cHoras = dnis.indexOf(dni)
 
     // como el metodo indexOf trae como valor -1 en caso de que no encuentre el valor dentro del array, se crea un if para que solo se cargue la info en caso de haber coincidencia
     if (cHoras !== -1) {
@@ -114,16 +98,45 @@ function cantidadHoras() {
 // Funcion para liquidar sueldo
 function liquidarSueldo() {
     // se obtiene el dni de la persona a la que se le quiere liquidar el sueldo
-    var valor = document.getElementById("liquidarSueldo").value
+    var dni = document.getElementById("liquidarSueldo").value
     // se busca dentro del array al objeto que coincide con el valor ingresado
-    var persona = empleados.find(item => item.dni === valor);
+    var persona = empleados.find(item => item.dni === dni);
     // se calculan los items dentro del bono de sueldo
     var sueldoBruto = persona.vHora * persona.cHoras
-    var descuentos = (persona.vHora * persona.cHoras) * 0.17
-    var sueldoNeto = (persona.vHora * persona.cHoras) * 0.83
+    var calcularDescuentos = (persona.vHora * persona.cHoras) * 0.17
+    var descuentos = calcularDescuentos.toFixed(2)
+    var calcularSueldoNeto = (persona.vHora * persona.cHoras) * 0.83
+    var sueldoNeto = calcularSueldoNeto.toFixed(2)
 
-    // se muestra el resultado de la liquidacion
-    alert("La liquidación de " + persona.nombre + " " + persona.apellido + " es:" + "\n Sueldo Bruto: $" + sueldoBruto + "\n Descuentos: $" + descuentos + "\n Sueldo Neto: $" + sueldoNeto)
+    // se guardan los valores
+    const liquidacion = { dni, sueldoBruto, descuentos, sueldoNeto }
+    const enJson = JSON.stringify(liquidacion)
 
+    localStorage.setItem("empleado", enJson)
+}
+
+// funcion para imprimir la liquidacion en pantalla
+function verLiquidacion() {
+    var employee = JSON.parse(localStorage.getItem('empleado'))
+
+    // se traen los elementos guardados
+    var dni = employee.dni
+    var sueldoBruto = employee.sueldoBruto
+    var descuentos = employee.descuentos
+    var sueldoNeto = employee.sueldoNeto
+
+    // se insertan los datos obtenidos dentro de la tabla
+    var table = document.getElementsByName("tablaliquidacion")[0];
+    var newRow = table.insertRow(1);
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+
+    cell1.innerHTML = dni;
+    cell2.innerHTML = "$ " + sueldoBruto;
+    cell3.innerHTML = "$ " + descuentos;
+    cell4.innerHTML = "$ " + sueldoNeto;
 }
 
